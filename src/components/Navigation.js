74864d93d52950c58238060ae6344f769a158cec
Naomi Rodriguez <naomi.rodriguez7@aol.com>
2020-01-28 17:22:37 -0500

    fixed navgations links to link in Nav.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,16 +7,19 @@ const Navigation = () => {
       <div className="App">
         <h1>React Router Mini</h1>
         <div>
-          <a href="/">Home</a>
+        
+          <Link to="/">Home</Link>
+          </div>
+
+          <div>
+           <Link to="/About">About</Link>
+           </div>
+           <div>
+        
+          <Link to="/Contact">Contact</Link>
         </div>
-        <div>
-          <a href="/about">About</a>
         </div>
-        <div>
-          <a href="/contact">Contact</a>
         </div>
-      </div>
-    </div>
   );
 };
 
